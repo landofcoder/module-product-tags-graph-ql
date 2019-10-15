3edbfa6ee1ab53d048fcaef522c75949d2a6201a
landofcoder <ecomteck@gmail.com>
2019-10-15 17:35:09 +0700

    updated module files

---
+++ Model/Resolver/DataProvider/Tag.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+declare(strict_types=1);
+namespace Lof\ProductTagsGraphQl\Model\Resolver\DataProvider;
+
+use Lof\ProductTags\Api\Data\TagInterface;
+use Lof\ProductTags\Api\TagRepositoryInterface;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+/**
+ * Lof Product Tags data provider
+ */
+
+class Tag
+{
+    /**
+     * @var TagRepositoryInterface
+     */
+    private $tagRepository;
+
+    /**
+     * @param \Lof\ProductTags\Api\TagRepositoryInterface $tagRepository
+     */
+    public function __construct(
+        \Lof\ProductTags\Api\TagRepositoryInterface $tagRepository
+    ) {
+        $this->tagRepository = $tagRepository;
+    }
+
+    /**
+     * @param int $tagId
+     * @return array
+     * @throws NoSuchEntityException
+     */
+    public function getData(int $tagId): array
+    {
+        $tag = $this->tagRepository->getById($tagId);
+
+        if (false === $tag->getStatus()) {
+            throw new NoSuchEntityException();
+        }
+
+        $tagData = [
+            TagInterface::TAG_ID => $tag->getTagId(),
+            TagInterface::TAG_STATUS => $tag->getStatus(),
+            TagInterface::TAG_TITLE => $tag->getTagTitle(),
+            TagInterface::TAG_IDENTIFIER => $tag->getIdentifier(),
+            TagInterface::TAG_DESCRIPTION => $tag->getTagDescription(),
+        ];
+        return $tagData;
+    }
+}
